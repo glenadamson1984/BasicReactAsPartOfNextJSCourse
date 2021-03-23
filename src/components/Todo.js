f7d0940c4acef27fb7d4f41ff68b29d5ffca1469
glenadamson1984 <glenadamson1984@gmail.com>
2021-03-23 19:45:23 +0000

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Todo = ({text}) => {
+
+  const deleteHandler = (event) => {
+    console.log("clicked")
+  }
+
+  return (
+    <div className="card">
+      <h2>{text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
