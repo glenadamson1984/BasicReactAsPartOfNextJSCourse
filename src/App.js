5cac65d5af22df6d321cef23fc36f7b654c03b29
glenadamson1984 <glenadamson1984@gmail.com>
2021-03-23 20:06:15 +0000

    deleting all files (basic intro), now to make meetup-app with routes etc

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import Todo from "./components/Todo"
-import Modal from "./components/Modal"
-import Backdrop from "./components/Backdrop"
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="learn react" />
-      <Todo text="advanced react" />
-      <Todo text="overview react" />
-      <Modal></Modal>
-      <Backdrop></Backdrop>
     </div>
   );
 }
