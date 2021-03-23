1701c117687adda7e7a202aae5c35e57448d5eb2
glenadamson1984 <glenadamson1984@gmail.com>
2021-03-23 20:46:34 +0000

    create the meetup app - routes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+import Allmeetups from "./pages/Allmeetups";
+import Favourites from "./pages/Favourites";
+import Newmeetups from "./pages/Newmeetups";
+import MainNavigation from './components/layout/MainNavigation'
+
 function App() {
   return (
     <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <Allmeetups />
+        </Route>
+        <Route path="/new-meetup">
+          <Newmeetups />
+        </Route>
+        <Route path="/favourites">
+          <Favourites />
+        </Route>
+      </Switch>
     </div>
   );
 }
